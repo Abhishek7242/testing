64699ac8588b908b2dab4f9d4504140d568d26b9
Abhishek Verma <131652685+Abhishek7242@users.noreply.github.com>
2026-07-02 11:17:07 +0530

    Delete laravel-core directory

--- laravel-core/resources/views/admin/layouts/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="p-6 border-t border-white/10 text-center text-slate-500 text-sm">
-    &copy; {{ date('Y') }} {{ config('app.name', 'Laravel') }}. All rights reserved.
-</footer>
--- laravel-core/resources/views/frontend/layouts/main.blade.php
+++
@@ -1,8 +0,0 @@
-@include('frontend.layouts.header')
-@include('frontend.layouts.navbar')
-
-<main>
-    @yield('content')
-</main>
-
-@include('frontend.layouts.footer')
